d261d4ac1cd19f47982c24ac19b28f15747433c1
Lukas Steinwender <lukas.steinwender99@gmail.com>
2026-07-27 14:19:17 +1000

    added count_inode

--- LuStCodeSnippets_sh/utils.sh
+++ LuStCodeSnippets_sh/utils.sh
@@ -56,7 +56,7 @@ agent_forwarding () {
         fi
     done
 
-    #default values for positional args    
+    #default values for positional args
     if [[ "${args[0]}" == "--help" ]]; then
         echo "$help"
     else
@@ -67,7 +67,7 @@ agent_forwarding () {
         echo "Setting up agent forwarding for github"
         eval "$(ssh-agent -s)"
         ssh-add ~/.ssh/github	#forward github sshkey
-    fi    
+    fi
 }
 
 calc () {
@@ -126,7 +126,7 @@ calc () {
         fi
     done
 
-    #default values for positional args  
+    #default values for positional args
 	local mexpr="${args[0]}"  		#math expression to evaluate
     local scale="${args[1]:-2}"	    #set scale
 
@@ -137,8 +137,73 @@ calc () {
         #FUNCTION BODY#
         ###############
 
-	    echo "scale=$scale; $mexpr" | bc      
-    fi        
+	    echo "scale=$scale; $mexpr" | bc
+    fi
+}
+
+count_inode() {
+    local help='
+        - function description
+
+        Usage
+        ```bash
+        count_inode dir1 dir2 ...
+        ```
+
+        Parameters
+        - $1+ (`dir1` `dir2` ...)
+            - `path` `path` ...
+            - directories to count contained files of
+                - will count recursively
+
+        Example
+        ```bash
+        count_inode .[a-z]*/    #counts all dot-directories
+        count_inode [^.]*/      #counts all non-dot-directories
+        ```
+
+        Output
+        ```bash
+        dir1: <number of files>
+        dir2: <number of files>
+        ...
+        ```
+    '
+
+    ################
+    #DEAL WITH ARGS#
+    ################
+    declare -a args=()  #positional args via array
+    declare -A kwargs=( #emulate named kwargs via associative array
+        [kwarg1]="val1"
+        [kwarg2]="val2"
+        [kwarg3]="val3"
+    )
+    for arg in "$@"; do
+        if [[ "$arg" == *=* ]]; then    #check if arg contains an `=` (kwargs)
+            key="${arg%%=*}"            #name (before `=`)
+            val="${arg#*=}"             #value (after `=`)
+            kwargs[$key]="$val"         #override default
+        else                            #interpret as positional
+            args+=("$arg")
+        fi
+    done
+
+    #default values for positional args
+    local arg1="${args[0]:-none}"
+    local arg2="${args[1]:-none}"
+
+    if [[ "${arg1}" == "--help" ]]; then
+        echo "$help"
+            return 0
+    fi
+
+    echo $"called count_inode" \
+        "${args[@]}"
+
+    for d in ${args[@]}; do
+        echo "$d: $(find "$d" -type f | wc -l); $(du "$d" -h --max-depth 0 | awk '{print $1}')";
+    done
 }
 
 #prevent direct execution
