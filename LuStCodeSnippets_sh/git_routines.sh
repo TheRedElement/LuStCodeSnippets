4bb28690f3588bff48c5e51557e7c38211ece509
Lukas Steinwender <lukas.steinwender99@gmail.com>
2026-08-17 09:10:07 +1000

    adjust bash function structure

--- LuStCodeSnippets_sh/git_routines.sh
+++ LuStCodeSnippets_sh/git_routines.sh
@@ -2,28 +2,26 @@
 
 git_log() {
     local help='
-        - function to print a prettified version of git log in the terminal
-            - formatting similar to a git graph
-        
+        prints prettified version of `git log` in the terminal
+
+        - formatting similar to a git graph
+
         Usage
-        -----
         ```bash
         git_log
         ```
-        
+
         Parameters
-        ----------
-        
+
         Example
-        -------
         ```bash
         git_log
         ```
-        
+
         Output
-        ------
         ```bash
-        ```    
+        [...]
+        ```
     '
 
     ################
@@ -46,41 +44,37 @@ git_log() {
     #default values for positional args
     if [[ "${args[0]}" == "--help" ]]; then
         echo "$help"
-    else
-        ###############
-        #FUNCTION BODY#
-        ###############
-
-        echo $"called git_log" \
-            ""
-
-        git log --graph --oneline --decorate --all --color
+        return 0
     fi
+
+    ############### #FUNCTION BODY#
+    ###############
+
+    echo $"called git_log" \
+        ""
+
+    git log --graph --oneline --decorate --all --color
 }
 
 rm_gitignored() {
     local help='
-        - function to delete all files in the current directory that are gitignored
-        
+        deletes all files in the current directory that are gitignored
+
         Usage
-        -----
         ```bash
         rm_gitignored
         ```
-        
+
         Parameters
-        ----------
-        
+
         Example
-        -------
         ```bash
         rm_gitignored
         ```
-        
+
         Output
-        ------
         ```bash
-        ```    
+        ```
     '
 
     ################
@@ -103,46 +97,43 @@ rm_gitignored() {
     #default values for positional args
     if [[ "${args[0]}" == "--help" ]]; then
         echo "$help"
-    else
-        ###############
-        #FUNCTION BODY#
-        ###############
-
-        echo $"called rm_gitignored" \
-            ""
-
-        git check-ignore *.* | xargs -r rm -v
+        return 0
     fi
+
+    ###############
+    #FUNCTION BODY#
+    ###############
+
+    echo $"called rm_gitignored" \
+        ""
+
+    git check-ignore *.* | xargs -r rm -v
 }
 
 unlink_submodule() {
     local help='
-        - function to unlik a git submodule
-        
+        unliks a git submodule
+
         Usage
-        -----
         ```bash
         unlink_submodule <submodule_path> [do_commit]
         ```
-        
+
         Parameters
-        ----------
         - $1 (`submodule_path`)
             - `string`
             - path to the submodule to unlink
         - $2 (`do_commit`)
             - `bool`, optional
             -  whether to add and commit the changes after unlinking the submodule
-        
+
         Example
-        -------
         ```bash
         ```
-        
+
         Output
-        ------
         ```bash
-        ```    
+        ```
     '
 
     ################
@@ -168,49 +159,50 @@ unlink_submodule() {
 
     if [[ "${args[0]}" == "--help" ]]; then
         echo "$help"
+        return 0
+    fi
+
+    ###############
+    #FUNCTION BODY#
+    ###############
+
+    echo $"called unlink_submodule" \
+        "${submodule_path} ${do_commit}"
+
+
+    #check if enough arguments are provided
+    if [ -z "$submodule_path" ]; then
+        echo "not enough arguments: unlink_submodule <submodule_path> [do_commit]"
+        return 1
+    fi
+
+    #ask user to confirm
+    read -p "Unlink submodule at '$submodule_path'? [y/N]: " confirm
+    if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
+        echo "Aborted."
+        return 1
+    fi
+    echo "Unlinking submodule at '$submodule_path'..."
+
+    #remove submodule entry from .gitmodules
+    git config -f .gitmodules --remove-section "submodule.$submodule_path"
+
+    #remove submodule entry from .git/config
+    git config -f .git/config --remove-section "submodule.$submodule_path"
+
+    #unstage the submodule
+    git rm --cached "$submodule_path"
+
+    #delete the submodule's .git directory if it exists
+    rm -rf "$submodule_path/.git"
+
+    #optionally add and commit the result
+    if [ "$do_commit" = true ]; then
+        git add "$submodule_path"
+        git commit -m "Convert submodule '$submodule_path' to regular directory"
+        echo "Submodule '$submodule_path' has been unlinked and committed as a regular directory."
     else
-        ###############
-        #FUNCTION BODY#
-        ###############
-
-        echo $"called unlink_submodule" \
-            "${submodule_path} ${do_commit}"
-
-
-        #check if enough arguments are provided
-        if [ -z "$submodule_path" ]; then
-            echo "not enough arguments: unlink_submodule <submodule_path> [do_commit]"
-            return 1
-        fi
-        
-        #ask user to confirm
-        read -p "Unlink submodule at '$submodule_path'? [y/N]: " confirm
-        if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
-            echo "Aborted."
-            return 1
-        fi
-        echo "Unlinking submodule at '$submodule_path'..."
-
-        #remove submodule entry from .gitmodules
-        git config -f .gitmodules --remove-section "submodule.$submodule_path"
-
-        #remove submodule entry from .git/config
-        git config -f .git/config --remove-section "submodule.$submodule_path"
-
-        #unstage the submodule
-        git rm --cached "$submodule_path"
-
-        #delete the submodule's .git directory if it exists
-        rm -rf "$submodule_path/.git"
-
-        #optionally add and commit the result
-        if [ "$do_commit" = true ]; then
-            git add "$submodule_path"
-            git commit -m "Convert submodule '$submodule_path' to regular directory"
-            echo "Submodule '$submodule_path' has been unlinked and committed as a regular directory."
-        else
-            echo "Submodule '$submodule_path' has been unlinked. Add and commit manually if needed."
-        fi
+        echo "Submodule '$submodule_path' has been unlinked. Add and commit manually if needed."
     fi
 }
 
--- LuStCodeSnippets_sh/publish_latex.sh
+++ LuStCodeSnippets_sh/publish_latex.sh
@@ -3,7 +3,8 @@
 publish_latex () {
 
     local help='
-        - compiles a publishable version of a paper
+        compiles a publishable version of a paper
+
         - will
             - create a new directory `${outdir}`
             - copy the original source file (`${infile}.tex) to a new, publishable file `${outdir}/${outfile}.tex`
@@ -71,82 +72,83 @@ publish_latex () {
 
     if [[ "${args[0]}" == "--help" ]]; then
         echo "$help"
-    else
-        ###############
-        #FUNCTION BODY#
-        ###############
-
-        echo $"called publish_latex" \
-            "${infile} ${outfile}" \
-            "outdir=${kwargs[outdir]}"
-
-        #extract figures
-        ##get all `\includegraphics[...]{...}` with contents`
-        ##extract contents from `{...}`
-        ##get filename and extension
-        ##convert to bash array
-        mapfile -t figures < <(
-            grep -oP '\\includegraphics(?:\[[^]]*\])?{[^}]+}' ${infile}.tex \
-            | grep -oP '(?<={)[^}]+' \
-            | grep -oP '(?<=gfx\/)\w+\.\w+$'
-        )
-
-        # printf "%s\n" "${figures[@]}"
-
-        #init submission directory
-        mkdir -p ${kwargs[outdir]};
-
-        #init submittable file
-        cp "${infile}.tex" "${kwargs[outdir]}${outfile}.tex";
-
-        #include bib-refs
-        cp bib-refs.bib "${kwargs[outdir]}/bib-refs.bib"
-        cp *.bst "${kwargs[outdir]}/"
-
-        #include latex class
-        cp *.cls "${kwargs[outdir]}/"
-
-        #include glossary
-        sed -i '/\input{glossary}/r glossary.tex' "${kwargs[outdir]}${outfile}.tex";
-        sed -i -E '/\\input\{glossary\}/d' "${kwargs[outdir]}${outfile}.tex";
-
-        #remove all comments
-        sed -i -E '/^\s*%/d' "${kwargs[outdir]}${outfile}.tex";                 #deletes commented lines (also indented ones)
-        sed -i -E 's/(^|[^\\])%.*$/\1/' "${kwargs[outdir]}${outfile}.tex";      #deletes line from (non-escaped) comment symbol onward #`\1` adds back matched prefix to not delete that character
-
-        #rename figures
-        for ((i=0; i<${#figures}; i++)); do
-            cp "gfx/${figures[i]}" "./${kwargs[outdir]}/fig$((i+0))_${figures[i]}";
-            sed -i -E "s/gfx\/${figures[i]}/fig$((i+0))_${figures[i]}/" "${kwargs[outdir]}${outfile}.tex";
-        done
-
-        #only operate in outdir (do not touch original directory)
-        cd "${kwargs[outdir]}"
-
-        #compile (4x to ensure `autonum`, `cleveref`, etc. resolve correctly)
-        pdflatex -interaction=nonstopmode "${outfile}.tex"  #1st pass (creates `.aux` for bibtex + glossary files)
-        bibtex "${outfile}"                                 #run BibTeX (uses `.aux`)
-        makeglossaries "${outfile}"                         #run makeglossaries
-        pdflatex -interaction=nonstopmode "${outfile}.tex"  #2nd pass (incorporates bibliography + glossary)
-        pdflatex -interaction=nonstopmode "${outfile}.tex"  #3rd pass (fix cross-references, cleveref, etc.)
-
-
-                #remove unnecessary auxiliary files
-        # rm -f *.bbl
-        rm -f *.acn *.acr *.alg *.aux *.bcf *.blg *.fdb_latexmk *.fls *.glg *.glo *.gls *.ist *.log *.out *.spl *.toc     #keep `.bbl` for arxiv
-
-        #for journal
-        #-----------
-        #zip latex source
-        rm "${outfile}_LatexSource.zip"
-        zip -r "${outfile}_LatexSource" *.bib *.bst *.cls *.tex fig*_*.*
-
-        #for arxiv
-        #---------
-        rm ax.tar
-        tar -cvvf ax.tar *.tex *.bst *.bbl *.cls fig*_*.*  #compress for upload to arxiv (only upload files that are necessary for compilation)
-
-        #go back to previous directory
-        cd -
+        return 0
     fi
+
+    ###############
+    #FUNCTION BODY#
+    ###############
+
+    echo $"called publish_latex" \
+        "${infile} ${outfile}" \
+        "outdir=${kwargs[outdir]}"
+
+    #extract figures
+    ##get all `\includegraphics[...]{...}` with contents`
+    ##extract contents from `{...}`
+    ##get filename and extension
+    ##convert to bash array
+    mapfile -t figures < <(
+        grep -oP '\\includegraphics(?:\[[^]]*\])?{[^}]+}' ${infile}.tex \
+        | grep -oP '(?<={)[^}]+' \
+        | grep -oP '(?<=gfx\/)\w+\.\w+$'
+    )
+
+    # printf "%s\n" "${figures[@]}"
+
+    #init submission directory
+    mkdir -p ${kwargs[outdir]};
+
+    #init submittable file
+    cp "${infile}.tex" "${kwargs[outdir]}${outfile}.tex";
+
+    #include bib-refs
+    cp bib-refs.bib "${kwargs[outdir]}/bib-refs.bib"
+    cp *.bst "${kwargs[outdir]}/"
+
+    #include latex class
+    cp *.cls "${kwargs[outdir]}/"
+
+    #include glossary
+    sed -i '/\input{glossary}/r glossary.tex' "${kwargs[outdir]}${outfile}.tex";
+    sed -i -E '/\\input\{glossary\}/d' "${kwargs[outdir]}${outfile}.tex";
+
+    #remove all comments
+    sed -i -E '/^\s*%/d' "${kwargs[outdir]}${outfile}.tex";                 #deletes commented lines (also indented ones)
+    sed -i -E 's/(^|[^\\])%.*$/\1/' "${kwargs[outdir]}${outfile}.tex";      #deletes line from (non-escaped) comment symbol onward #`\1` adds back matched prefix to not delete that character
+
+    #rename figures
+    for ((i=0; i<${#figures}; i++)); do
+        cp "gfx/${figures[i]}" "./${kwargs[outdir]}/fig$((i+0))_${figures[i]}";
+        sed -i -E "s/gfx\/${figures[i]}/fig$((i+0))_${figures[i]}/" "${kwargs[outdir]}${outfile}.tex";
+    done
+
+    #only operate in outdir (do not touch original directory)
+    cd "${kwargs[outdir]}"
+
+    #compile (4x to ensure `autonum`, `cleveref`, etc. resolve correctly)
+    pdflatex -interaction=nonstopmode "${outfile}.tex"  #1st pass (creates `.aux` for bibtex + glossary files)
+    bibtex "${outfile}"                                 #run BibTeX (uses `.aux`)
+    makeglossaries "${outfile}"                         #run makeglossaries
+    pdflatex -interaction=nonstopmode "${outfile}.tex"  #2nd pass (incorporates bibliography + glossary)
+    pdflatex -interaction=nonstopmode "${outfile}.tex"  #3rd pass (fix cross-references, cleveref, etc.)
+
+
+            #remove unnecessary auxiliary files
+    # rm -f *.bbl
+    rm -f *.acn *.acr *.alg *.aux *.bcf *.blg *.fdb_latexmk *.fls *.glg *.glo *.gls *.ist *.log *.out *.spl *.toc     #keep `.bbl` for arxiv
+
+    #for journal
+    #-----------
+    #zip latex source
+    rm "${outfile}_LatexSource.zip"
+    zip -r "${outfile}_LatexSource" *.bib *.bst *.cls *.tex fig*_*.*
+
+    #for arxiv
+    #---------
+    rm ax.tar
+    tar -cvvf ax.tar *.tex *.bst *.bbl *.cls fig*_*.*  #compress for upload to arxiv (only upload files that are necessary for compilation)
+
+    #go back to previous directory
+    cd -
 }
